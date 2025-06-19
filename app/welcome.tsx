11a1c2259d9a47cc641bbba9bf64a1596b33fd2d
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-06-19 10:27:45 +0530

    master

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,9 +1,3 @@
-import CustomButton from '@/components/CustomButton';
-import { ThemedView } from '@/components/ThemedView';
-import { Ionicons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import { Text } from 'react-native';
-import Animated, { FadeInDown } from 'react-native-reanimated';
 
 export default function WelcomeScreen() {
   return null;
